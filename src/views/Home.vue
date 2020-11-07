4cccd7987c61e3c16235c8759f8bab0ece7e23d3
yuyi.shi <yuyi.shi@scienjoy.com>
2020-11-07 19:13:24 +0800

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <tree msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import tree from '@/components/tree.vue'; // @ is an alias to /src
 
 @Options({
   components: {
-    HelloWorld,
+    tree,
   },
 })
 export default class Home extends Vue {}
